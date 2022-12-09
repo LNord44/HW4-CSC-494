ff8aba977b519a58372a38df57bfa247095905ef
LNord44 <81250830+LNord44@users.noreply.github.com>
2022-12-08 15:11:43 -1000

    Update ticketsale.test.js

--- ticketsale.test.js
+++ ticketsale.test.js
@@ -4,19 +4,16 @@ const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 const {abi, bytecode} = require('../compile');
 
-beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
-    lottery = await new web3.eth.Contract(abi)
-      .deploy({ data: bytecode })
-      .send({ from: accounts[0], gas: "1000000" });
-  });
+let ticketsale;
+let accounts;
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-    ottery = await new web3.eth.Contract(abi)
+    ticketsale = await new web3.eth.Contract(abi)
       .deploy({
         data: bytecode,
+        arguments: [100,1],
       })
       .send({ from: accounts[0], gasPrice: 8000000000, gas: 4700000});
   });
@@ -26,8 +23,44 @@ beforeEach(async () => {
       assert.ok(ticketsale.options.address);
     });
     it("verify purchase", async () => {
-        ticektsale.methods.buyTicket(2).send({from: accounts[1], value: 1, gasPrice: 150000, gas: 477000});
-        const ticketId =await ticketSale.methods.getTicketID(accounts([1]).call());
-        //assert.equal(ticketId,2);
+        ticketsale.methods.buyTicket(2).send({from: accounts[1], value: 1, gasPrice: 8000000000, gas: 4700000});
+        const ticketId =await ticketsale.methods.getTicketID(accounts[1]).call();
+        assert.equal(ticketId,2);
     })
   });
+
+  /*
+  describe("Ticket Sale", () => {
+    it("deploys a contract with owner", () => {
+      assert.ok(ticketsale.options.address);
+    });
+    it("verify purchase", async () => {
+        ticketsale.methods.buyTicket(2).send({from: accounts[1], value: 1, gasPrice: 8000000000, gas: 4700000});
+        const ticketId =await ticketsale.methods.getTicketID(accounts[1]).call();
+        assert.equal(ticketId,2);
+    })
+  });
+  */
+
+  /*
+  describe("Ticket Sale", () => {
+    it("deploys a contract with owner", () => {
+      assert.ok(ticketsale.options.address);
+    });
+    it("verify purchase", async () => {
+        ticketsale.methods.buyTicket(2).send({from: accounts[1], value: 1, gasPrice: 8000000000, gas: 4700000});
+        const ticketId =await ticketsale.methods.getTicketID(accounts[1]).call();
+        assert.equal(ticketId,2);
+    })
+  });
+  */
+
+  // . send    constructor, buyTicket then getticketId to verify, offer and accept swap
+  // . call     getTicketOf,
+  //it("returns ticket number owened by address", async () =>{
+  //})
+  // one person can't buy multiple
+  // one ticket can't be sold twice.
+  // offer swap
+    // call offer swap, call accept swap, get ticket of person #1, #2 to verify
+    // assert.equal(ticketId,5) for after swap
